0aca2aa3d4e0b1bb5974ffdeccbeecebb1a69458
beastcode9999 <beastcode9999@gmail.com>
2026-04-05 17:05:24 +0930

    feat: hide secret key

--- apps/companion/apps.py
+++ apps/companion/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class CompanionConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'companion'
+    name = 'apps.companion'
--- apps/mood/apps.py
+++ apps/mood/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class MoodConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'mood'
+    name = 'apps.mood'
