d8c914f633ef84f7fa602c4d1524f7ba02efbc5d
beastcode9999 <beastcode9999@gmail.com>
2026-04-09 11:10:43 +0930

    feat: add login endpoint

--- apps/users/views.py
+++ apps/users/views.py
@@ -2,9 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 from rest_framework import status
-from rest_framework.response import Response 
+from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.exceptions import TokenError
+from django.contrib.auth import authenticate
 from .serializers import RegisterSerializer, UserSerializer
 
 class RegisterView(APIView):
@@ -34,3 +37,35 @@ class RegisterView(APIView):
                 'refresh': str(refresh),
             }
         }, status=status.HTTP_201_CREATED)
+    
+class LoginView(APIView):
+    """
+    POST api/users/login/
+    Accepts email and password
+    Returns user data with fresh JWT tokens when successful
+    Authenticate users and return user object
+    """
+
+    def post(self, request):
+        email = request.data.get('email')
+        password = request.data.get('password')
+
+        # use authenticate to compare hashed password
+        user = authenticate(request, username=email, password=password)
+
+        if not user:
+            return Response(
+                {'error' : 'Invalid credentials'},
+                status=status.HTTP_401_UNAUTHORIZED
+            )
+        
+        # if user 
+        refresh = RefreshToken.for_user(user)
+
+        return Response({
+            'user': UserSerializer(user).data,
+            'tokens': {
+                'access': str(refresh.access_token),
+                'refresh': str(refresh),
+            }
+        })
